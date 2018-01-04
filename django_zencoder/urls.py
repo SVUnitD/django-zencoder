d6cdb466f3708ae85a2b6af658bedae52aa2d092
Simon Liedtke <simon.liedtke@gmail.com>
2018-01-04 15:03:41 +0000

    Django1.11 compatibility

--- django_zencoder/urls.py
+++ django_zencoder/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'django_zencoder.views',
-    url(r'notify/', 'notification', name='zencoder_notification'),
-)
+from .views import notification
+
+urlpatterns = [
+    url(r'notify/', notification, name='zencoder_notification'),
+]
